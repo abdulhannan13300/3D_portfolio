4d7913522b9a0e7652d41b532c56df41c77f4002
NoOne <abdul.hannan13300@gmail.com>
2023-08-22 09:04:31 +0530

    Hero Section 1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        {/* The circle and line with the violet gradient */}
+        <div className="flex flex-col justify-center items-center mt-5">
+          {/* small circle */}
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          {/* The violet gradient line */}
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
 
-export default Hero
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Abdul Hannan</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop web applications, user interfaces
+            <br className="sm:block hidden" /> and landing pages
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
+
+export default Hero;
